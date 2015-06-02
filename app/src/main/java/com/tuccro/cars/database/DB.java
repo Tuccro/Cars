3a9893384d608659ff06e7962480c9a53975c25e
Tuccro <tuccro@yahoo.com>
2015-06-02 09:44:57 +0300

    db connection

--- app/src/main/java/com/tuccro/cars/database/DB.java
+++ app/src/main/java/com/tuccro/cars/database/DB.java
@@ -1,6 +1,29 @@
 package com.tuccro.cars.database;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
 public class DB {
 
+    private static final String DB_NAME = "cars.sqlite";
+    private static final int DB_VERSION = 1;
+
+    private final Context context;
+
+
+    private DBHelper dbHelper;
+    private SQLiteDatabase db;
+
+    public DB(Context context) {
+        this.context = context;
+    }
+
+    public void open() {
+        dbHelper = new DBHelper(context, DB_NAME, null, DB_VERSION);
+        db = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        if (dbHelper != null) dbHelper.close();
+    }
 }
