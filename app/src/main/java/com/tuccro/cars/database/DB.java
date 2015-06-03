5649c7e9890fd3965466ebb2da52a3c8a5142dc6
Tuccro <tuccro@yahoo.com>
2015-06-03 16:46:01 +0300

    DB methods

--- app/src/main/java/com/tuccro/cars/database/DB.java
+++ app/src/main/java/com/tuccro/cars/database/DB.java
@@ -1,10 +1,11 @@
 package com.tuccro.cars.database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
-public class DB implements IDBStrings{
+public class DB implements IDBStrings {
 
     private final Context context;
 
@@ -23,6 +24,48 @@ public class DB implements IDBStrings{
 
     public void close() {
         if (dbHelper != null) dbHelper.close();
+    }
+
+    public void addBrand(String name) {
+        ContentValues cv = new ContentValues();
+        cv.put(BRAND_NAME, name);
+        db.insert(DB_TABLE_BRAND, null, cv);
+    }
+
+    public void deleteBrand(int id) {
+        db.delete(DB_TABLE_BRAND, BRAND_ID + " = " + id, null);
+    }
+
+    public void addModel(int idBrand, String name, int startYear, int endYear) {
+        ContentValues cv = new ContentValues();
+        cv.put(MODEL_BRAND_ID, idBrand);
+        cv.put(MODEL_NAME, name);
+        cv.put(MODEL_START_YEAR, startYear);
+        cv.put(MODEL_END_YEAR, endYear);
+        db.insert(DB_TABLE_MODEL, null, cv);
+    }
+
+    public void addModel(int idBrand, String name, int startYear) {
+        ContentValues cv = new ContentValues();
+        cv.put(MODEL_BRAND_ID, idBrand);
+        cv.put(MODEL_NAME, name);
+        cv.put(MODEL_START_YEAR, startYear);
+        db.insert(DB_TABLE_MODEL, null, cv);
+    }
+
+    public void deleteModel(int id) {
+        db.delete(DB_TABLE_MODEL, MODEL_ID + " = " + id, null);
+    }
+
+    public void addEngine(int idModel, String name) {
+        ContentValues cv = new ContentValues();
+        cv.put(ENGINE_MODEL_ID, idModel);
+        cv.put(ENGINE_NAME, name);
+        db.insert(DB_TABLE_ENGINE, null, cv);
+    }
+
+    public void deleteEngine(int id) {
+        db.delete(DB_TABLE_ENGINE, ENGINE_ID + " = " + id, null);
     }
 
     public Cursor getAllBrands() {
